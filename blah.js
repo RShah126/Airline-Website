82f3c29c9de80b1efcb6ce1e2e606a059f81149b
Ryan Shah <65997615+RShah126@users.noreply.github.com>
2022-08-08 00:21:03 -0400

    Update and rename blah.js to readthis.js

--- blah.js
+++
@@ -1,1 +0,0 @@
-console.log("Thanks for checking out this repository, unfortunately the code cannot be shared, and I have also lost my code for it :/");
---
+++ readthis.js
@@ -0,0 +1,2 @@
+console.log("Thanks for checking out this repository, unfortunately the code cannot be shared, and I have also lost my code for it.");
+console.log("However, feel free to check out the video of the project.");
