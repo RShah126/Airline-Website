e73b6634a84b07874f2e70ba1fa2ae1660149591
Ryan Shah <65997615+RShah126@users.noreply.github.com>
2023-09-11 23:31:43 -0400

    Delete readthis.js

--- readthis.js
+++
@@ -1,2 +0,0 @@
-console.log("Thanks for checking out this repository, unfortunately the code cannot be shared, and I have also lost my code for it.");
-console.log("However, feel free to check out the video of the project.");
